24d31b04033b422ce5893ffa845276c9bcb9e8af
Codingway-Amber <amberreine7@gmail.com>
2024-11-06 02:57:33 -0600

    complete

--- sketch.js
+++ sketch.js
@@ -1,37 +1,35 @@
-let paint1, paint2, paint3, paint4, paint5;
+let paint1, paint2, paint3, paint4, paint5, paint6, paint7, paint8;
 let img
 
 function preload() {
-  img = loadImage("slackprofilepic2.png")
+  img = loadImage("me.png")
 }
 
 function setup() {
   createCanvas(800, 400);
   noStroke();
 
-  image(img, 170, 210, 100, 100);
+  image(img, 200, 140, 400, 250);
 
   paint1 = new Paint(color(random(255), random(255), random(255)), random(1, 3));
   paint2 = new Paint(color(random(255), random(255), random(255)), random(1, 3));
   paint3 = new Paint(color(random(255), random(255), random(255)), random(1, 3));
   paint4 = new Paint(color(random(255), random(255), random(255)), random(1, 3));
   paint5 = new Paint(color(random(255), random(255), random(255)), random(1, 3));
+  paint6 = new Paint(color(random(255), random(255), random(255)), random(1, 3));
+  paint7 = new Paint(color(random(255), random(255), random(255)), random(1, 3));
+  paint8 = new Paint(color(random(255), random(255), random(255)), random(1, 3));
 }
 
 function draw() {
-  // background(200);
-
   paint1.display();
   paint2.display();
   paint3.display();
   paint4.display();
   paint5.display();
-
-  // push();
-  // fill(0);
-  // text(mouseX, 0, 370); 
-  // text(mouseY, 0, 390);
-  // pop();
+  paint6.display();
+  paint7.display();
+  paint8.display();
 }
 
 
