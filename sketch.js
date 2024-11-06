8f9ad7734563c7a11ec0514d6990d31c24cec18c
Codingway-Amber <amberreine7@gmail.com>
2024-11-06 01:32:51 -0600

    test image

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
 let paint1, paint2, paint3, paint4, paint5;
+let img
+
+function preload() {
+  img = loadImage("slackprofilepic2.png")
+}
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
   noStroke();
+
+  image(img, 170, 210, 100, 100);
 
   paint1 = new Paint(color(random(255), random(255), random(255)), random(1, 3));
   paint2 = new Paint(color(random(255), random(255), random(255)), random(1, 3));
@@ -19,6 +26,12 @@ function draw() {
   paint3.display();
   paint4.display();
   paint5.display();
+
+  // push();
+  // fill(0);
+  // text(mouseX, 0, 370); 
+  // text(mouseY, 0, 390);
+  // pop();
 }
 
 
@@ -26,7 +39,7 @@ function Paint(tempPaintColor, speed) {
   this.paintColor = tempPaintColor;
   this.paintXposition = random(width); 
   this.paintYposition = 0; 
-  this.paintRadius = 50;
+  this.paintRadius = 35;
   this.speed = speed; 
 
   this.display = function() {
